dcabd07f137fb282430fa07d30116bf3682a1102
Welyab Paula <welyab.exe@gmail.com>
2019-06-28 23:41:11 -0300

    initial commit

---
+++ spring-boot-reactive-programming/src/main/java/dev/welyab/tutorials/springbootreactiveprogramming/SpringBootReactiveProgrammingApplication.java
@@ -0,0 +1,13 @@
+package dev.welyab.tutorials.springbootreactiveprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactiveProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactiveProgrammingApplication.class, args);
+	}
+
+}
